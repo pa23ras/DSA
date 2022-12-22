bcf51bab3d562e000b463f5bc952e771e40e0635
pa23ras <121255086+pa23ras@users.noreply.github.com>
2022-12-22 23:55:24 +0530

    Create sum_of_n_num.java

---
+++ sum_of_n_num.java
@@ -0,0 +1,42 @@
+yuyeriye
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//your code here
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int k = scn.nextInt();//rotations
+    int q = scn.nextInt();
+    
+    int[] arr = new int[n];
+    for(int i = 0; i < n; i++){
+      arr[i] = scn.nextInt();
+    }
+    
+    int[] queries = new int[q];
+    for(int i = 0; i < q; i++){
+      queries[i] = scn.nextInt();
+    }
+    
+  ////Actual code start
+    int[] ansArr = new int[n];
+    for(int i = 0; i < n; i++){
+      int newIdx = (i + k)%n;
+      ansArr[newIdx] = arr[i];
+    }
+    
+    for(int i = 0; i < queries.length; i++){
+      int idx = queries[i];
+      System.out.print(ansArr[idx] + " ");
+    }
+    
+	}
+}
+
+
